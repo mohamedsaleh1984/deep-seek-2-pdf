9c5a27ebea5d5929fe35d73a3640e40d9b12cffd
Mohamed <mohamedsaleh1984@hotmail.com>
2025-02-01 22:22:59 -0500

    e

--- background.js
+++ background.js
@@ -1,16 +1,20 @@
 const icons = {
-    enabled: "./pdf.png",
-    disabled: "./pdf-gs.png"
-  };
- 
+  enabled: "pdf.png",
+  disabled: "pdf-gs.png"
+};
 
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    console.log(tab.url)
-    if (tab.url.includes("https://chat.deepseek.com/")) {
-      chrome.browserAction.setIcon({ tabId, path: icons.enabled });
-      chrome.browserAction.setPopup({ popup: "DeepSeek2pdf.html" }); // your popup filename
-    } else {
-      chrome.browserAction.setIcon({ tabId, path: icons.disabled });
-      chrome.browserAction.setPopup({ popup: "" });
-    };
+chrome.tabs.onActivated.addListener((info) => {
+  // console.log(tab.url)
+  // const tabId = info.tabId
+  // if (info.url.includes("https://chat.deepseek.com")) {
+  //   chrome.browserAction.setIcon({ tabId, path: icons.enabled });
+  //   chrome.browserAction.setPopup({ popup: "DeepSeek2pdf.html" }); // your popup filename
+  // } else {
+  //   chrome.browserAction.setIcon({ tabId, path: icons.disabled });
+  //   chrome.browserAction.setPopup({ popup: "" });
+  // };
+
+  var tab = chrome.tabs.get(info.tabId, function (tab) {
+    localStorage["current_url"] = tab.url;
   });
+});
